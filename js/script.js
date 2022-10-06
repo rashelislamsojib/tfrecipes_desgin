b1dcf1cea899b01e1b9db2a6cf1d51570ee9a7fd
RashelIslamSojib <mdrashelislam86@gmail.com>
2022-10-06 12:05:31 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown_menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
+$(document).ready (function(){
+    //toggle menu /navbar script
+    
+    $('.main_header .menu-btn').click(function(){
+        $('.main_header .menu').toggleClass("active");
+          $('.main_header .menu-btn i').toggleClass("active");      // body...
+      });
+
+//         if(this.scrollY > 20){
+//          $('.navbar').addClass("sticky");
+//         }
+//         else{
+//           $('.navbar').removeClass("sticky");
+//         }
+               
+//         // scroll-up button show/hide script
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
+
+
+//     // slide-up script
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         // removing smooth scroll on slide-up button click
+//         $('html').css("scrollBehavior", "auto");
+//     });
+    
+//     //toggle menu /navbar script
+    
+//     $('.menu-btn').click(function(){
+//       $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");      // body...
+//     });
+
+//   // typing animation stying
+//   var typed = new Typed(".typing",{
+//       strings:["YouTuber", "Developer", "Blogger", "Freelancer"],
+//       typeSpeed: 100,
+//       backSpeed: 60,
+//       loop: true 
+
+//   });
+
+//      //owl carousel script
+//      $('.carousel').owlCarousel({
+//       margin: 20,
+//       loop: true,
+//       autoplay: true,
+//       autoplayTimeOut: 2000,
+//       autoplayHoverPause: true,
+//       responsive:{
+//         0:{
+//           items: 1,
+//           nav: false
+//         },
+//         600:{
+//           items: 2,
+//           nav: false
+//         },
+//         1000:{
+//           items: 3,
+//           nav: false
+//         }
+//       }
+//      });
+});
